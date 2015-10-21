07d713677efbc614eedb00d161c5b0895b219197
Andrew Wong-Rolle <awongrolle@college.harvard.edu>
2015-10-21 17:48:38 -0400

    matrix version of qft works

--- shor_python/qft.py
+++ shor_python/qft.py
@@ -4,13 +4,20 @@
 import numpy as np
 import math
 import cmath
-def qft():
+def qft(in_qubits):
     # dimension of the QFT
-    # M = len(in_qubits)
-    M = 512
+    M = len(in_qubits)
+    print M 
     p = complex(0, 2*math.pi / M)
     omega = cmath.exp(p)
-    print omega
-    return
+    print 'omega is %05f %+05fi' % (omega.real, omega.imag)
+    transform = np.ones((M,M), dtype=complex)    
+    for x in xrange(M):
+        for y in xrange(M):
+            transform[x][y] = pow(omega,x*y)
+            print 'x = %d, y = %d, omega^xy = %05f %+05fi' % (x,y,transform[x][y].real, transform[x][y].imag)
+    print(transform)
+    transform = transform * (1 / math.sqrt(M))
+    return np.dot(transform,in_qubits)
     
-qft()    
+
