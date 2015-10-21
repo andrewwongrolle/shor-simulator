cae0a8c22b659d81c54fada501f2ba8483af6018
Andrew Wong-Rolle <awongrolle@college.harvard.edu>
2015-10-21 16:48:48 -0400

    trivial implementation of qft working

--- shor_python/qft.py
+++ shor_python/qft.py
@@ -1,7 +1,16 @@
 # Matrix implementation of quantum fourier transform.
-# Takes in a normalized ket.
+# Takes in a normalized ket implemented as an np array.
 
-def qft(in_qubits):
+import numpy as np
+import math
+import cmath
+def qft():
     # dimension of the QFT
-    M = len(in_qubits)
-
+    # M = len(in_qubits)
+    M = 512
+    p = complex(0, 2*math.pi / M)
+    omega = cmath.exp(p)
+    print omega
+    return
+    
+qft()    
