02169028ae7169196dc5660720f2fe1102b5d923
Andrew Wong-Rolle <awongrolle@college.harvard.edu>
2015-11-09 17:36:08 -0500

    in place measure, classical portion of shor's algorithm implemented

--- shor_python/qft.py
+++ shor_python/qft.py
@@ -1,23 +1,21 @@
-# Matrix implementation of quantum fourier transform.
+# Matrix implementation of the quantum fourier transform.
 # Takes in a normalized ket implemented as an np array.
 
 import numpy as np
 import math
 import cmath
+
 def qft(in_qubits):
     # dimension of the QFT
     M = len(in_qubits)
-    print M 
+    # the Mth root of unity
     p = complex(0, 2*math.pi / M)
     omega = cmath.exp(p)
-    print 'omega is %05f %+05fi' % (omega.real, omega.imag)
+    # the ijth entry of the QFT matrix is omega^((i-1)(j-1))
     transform = np.ones((M,M), dtype=complex)    
     for x in xrange(M):
         for y in xrange(M):
-            transform[x][y] = pow(omega,x*y)
-            print 'x = %d, y = %d, omega^xy = %05f %+05fi' % (x,y,transform[x][y].real, transform[x][y].imag)
-    print(transform)
-    transform = transform * (1 / math.sqrt(M))
+            transform[x][y] = (1 / math.sqrt(M)) * pow(omega,x*y)
     return np.dot(transform,in_qubits)
     
 
