cf29a0be2d56e82af425e0fad6f69f1a34ba2cd7
Andrew Wong-Rolle <awongrolle@college.harvard.edu>
2015-11-10 20:09:15 -0500

    sample qubits finished

--- shor_python/qgates.py
+++ shor_python/qgates.py
@@ -1,31 +1,29 @@
-# Lots of quantum gates
+# Quantum gates.
 # Assumes input qubits are arrays of two complex numbers.
 
 from math import sqrt
-
+import numpy as np
 # NOT gate
-    def qnot(q):
-        return [q[0],q[1]]
+def qnot(q):
+    return np.array([q[1],q[0]], dtype = complex)
 
 # Hadamard
-    def hadamard(in_qubits):
-        return [sqrt(0.5)*(q[0]+q[1]),sqrt(0.5)*(q[0]-q[1]]
+def hadamard(q):
+    return np.array([sqrt(0.5)*(q[0]+q[1]),sqrt(0.5)*(q[0]-q[1])], dtype = complex)
 
 # Rotate
 # takes in theta as angle of rotation in radians
-    def rotate(q,theta):
-        return [(cos(theta)*q[0]-sin(theta)*q[1]),(sin(theta)*q[0]+cos(theta)*q[1])]
+def rotate(q,theta):
+    return np.array([cos(theta)*q[0]-sin(theta)*q[1],sin(theta)*q[0]+cos(theta)*q[1]], dtype = complex)
 
 # Phase flip (Z)
 # acts as NOT gate in the plus/minus basis
-    def phase_flip(q):
-        return [q[0],-q[1]]
+def phase_flip(q):
+    return np.array([q[0],-q[1]], dtype = complex)
 
 # CNOT
 # takes in two-qubit system vector
 # performs NOT gate on target vector if first qubit is in state 1
-    def CNOT(q):
-        return [q[0],q[1],q[3],q[2]]
+def CNOT(q):
+    return np.array([q[0],q[1],q[3],q[2]], dtype = complex)
         
-
-
--- shor_python/sample_qubits.py
+++ shor_python/sample_qubits.py
@@ -3,22 +3,26 @@
 import numpy as np
 from math import sqrt
 
+# two dimensional standard basis for one qubit system
+zero = np.array([1,0], dtype = complex)
+one = np.array([0,1], dtype = complex)
+
+# plus/minus basis vectors for one qubit system
+plus = (1 / sqrt(2)) * np.array([1, 1], dtype = complex)
+minus = (1 / sqrt(2)) * np.array([1,-1], dtype = complex)
+
 # four dimensional basis vectors for two qubits system
 e1 = np.array([1,0,0,0], dtype = complex) # |00>
 e2 = np.array([0,1,0,0], dtype = complex) # |01>
 e3 = np.array([0,0,1,0], dtype = complex) # |10>
 e4 = np.array([0,0,0,1], dtype = complex) # |11>
 
-# plus/minus basis vectors for one qubits systems
-plus = (1 / sqrt(2)) * np.array([1, 1], dtype = complex)
-minus = (1 / sqrt(2)) * np.array([1,-1], dtype = complex)
-
 # Bell states
 #  1 / sqrt(2) * |01> + |10>
-psi_plus = 1 / sqrt(2) * np.array([0,1,0,1], dtype = complex) 
+psi_plus = 1 / sqrt(2) * np.array([0,1,1,0], dtype = complex) 
 
 # 1 / sqrt(2) * |01> - |10>
-psi_minus = 1 / sqrt(2 ) * np.array([0,1,0,-1], dtype = complex)   
+psi_minus = 1 / sqrt(2 ) * np.array([0,1,-1,0], dtype = complex)   
 
 # 1 / sqrt(2) * |00> + |11>
 phi_plus = 1 / sqrt(2 ) * np.array([1,0,0,1], dtype = complex)   
